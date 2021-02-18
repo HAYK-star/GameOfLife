ca1b43742af8617b447c32bced99dcf117cd6f69
HAYK-star <hayk.mkrtchyan5.y@tumo.org>
2021-02-18 19:28:04 +0400

    Final Project

--- script.js
+++ script.js
@@ -1,120 +1,61 @@
-let matrix = [];
-let side = 8;
-let grassArr = [];
-let grassEaterArr = [];
-let predatorArr = [];
-let lavaArr = [];
-let hrshejArr = [];
 
+//! Setup function fires automatically
 function setup() {
-    matrixGenerator(100, 20, 15, 10, 5, 10);
-    createCanvas(matrix[0].length * side, matrix.length * side);
-    background('grey');
-    frameRate(8);
 
-    noStroke()
+    var socket = io();
 
-    function matrixGenerator(matrixSize, grassCount, grassEaterCount, predatorCount, lavaCount, hrshejCount) {
-        for (let index = 0; index < matrixSize; index++) {
-            matrix[index] = [];
-            for (let i = 0; i < matrixSize; i++) {
-                matrix[index][i] = 0;
-            }
-        }
-        for (let index = 0; index < grassCount; index++) {
-            let x = Math.floor(random(0, matrixSize));
-            let y = Math.floor(random(0, matrixSize));
-            matrix[y][x] = 1;
-        }
-        for (let index = 0; index < grassEaterCount; index++) {
-            let x = Math.floor(random(0, matrixSize));
-            let y = Math.floor(random(0, matrixSize));
-            matrix[y][x] = 2;
-        }
-        for (let index = 0; index < predatorCount; index++) {
-            let x = Math.floor(random(0, matrixSize));
-            let y = Math.floor(random(0, matrixSize));
-            matrix[y][x] = 3;
-        }
-        for (let index = 0; index < lavaCount; index++) {
-            let x = Math.floor(random(0, matrixSize));
-            let y = Math.floor(random(0, matrixSize));
-            matrix[y][x] = 4;
-        }
-        for (let index = 0; index < hrshejCount; index++) {
-            let x = Math.floor(random(0, matrixSize));
-            let y = Math.floor(random(0, matrixSize));
-            matrix[y][x] = 5;
-        }
-    }
+    var side = 30;
 
-    for (let y = 0; y < matrix.length; y++) {
-        for (let x = 0; x < matrix[y].length; x++) {
-            if (matrix[y][x] == 1) {
-                let grass = new Grass(x, y);
-                grassArr.push(grass);
-            }
-            else if (matrix[y][x] == 2) {
-                let grassEater = new GrassEater(x, y);
-                grassEaterArr.push(grassEater);
-            }
-            else if (matrix[y][x] == 3) {
-                let predator = new Predator(x, y);
-                predatorArr.push(predator);
-            }
-            else if (matrix[y][x] == 4) {
-                let lava = new Lava(x, y);
-                lavaArr.push(lava);
-            }
-            else if (matrix[y][x] == 5) {
-                let hrshej = new Hrshej(x, y);
-                hrshejArr.push(hrshej);
+    var matrix = [];
+
+    //! Getting DOM objects (HTML elements)
+    let grassCountElement = document.getElementById('grassCount');
+    let grassEaterCountElement = document.getElementById('grassEaterCount');
+    let predatorCountElement = document.getElementById('predatorCount');
+    let lavaCountElement = document.getElementById('lavaCount');
+    let hrshejCountElement = document.getElementById('hrshejCount');
+
+    //! adding socket listener on "data" <-- name, after that fire 'drawCreatures' function 
+
+    socket.on("data", drawCreatures);
+
+    function drawCreatures(data) {
+        //! after getting data pass it to matrix variable
+        matrix = data.matrix;
+        grassCountElement.innerText = data.grassCounter;
+        grassEaterCountElement.innerText = data.grassEaterCounter;
+        predatorCountElement.innerText = data.predatorCounter;
+        lavaCountElement.innerText = data.lavaCounter;
+        hrshejCountElement.innerText = data.hrshejCounter;
+        //! Every time it creates new Canvas woth new matrix size
+        createCanvas(matrix[0].length * side, matrix.length * side)
+        //! clearing background by setting it to new grey color
+        background('#acacac');
+        //! Draw grassCount and grassEaterCount to HTML (use DOM objects to update information, yes, and use .innerText <- function)
+
+        //! Drawing and coloring RECTs
+        for (var i = 0; i < matrix.length; i++) {
+            for (var j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == 1) {
+                    fill("green");
+                    rect(j * side, i * side, side, side);
+                } else if (matrix[i][j] == 2) {
+                    fill("yellow");
+                    rect(j * side, i * side, side, side);
+                } else if (matrix[i][j] == 0) {
+                    fill('#acacac');
+                    rect(j * side, i * side, side, side);
+                } else if (matrix[i][j] == 3) {
+                    fill('red');
+                    rect(j * side, i * side, side, side);
+                } else if (matrix[i][j] == 4) {
+                    fill('orange');
+                    rect(j * side, i * side, side, side);
+                } else if (matrix[i][j] == 5) {
+                    fill("black");
+                    rect(j * side, i * side, side, side);
+                }
             }
         }
-    }
-
-}
-
-function draw() {
-
-    for (let y = 0; y < matrix.length; y++) {
-        const element = matrix[y];
-        for (let x = 0; x < element.length; x++) {
-
-            if (matrix[y][x] == 1) {
-                fill('green')
-            }
-            else if (matrix[y][x] == 2) {
-                fill('yellow')
-            }
-            else if (matrix[y][x] == 3) {
-                fill('red')
-            }
-            else if (matrix[y][x] == 4) {
-                fill('orange')
-            }
-            else if (matrix[y][x] == 5) {
-                fill('black')
-            }
-            else {
-                fill('grey')
-            }
-            rect(x * side, y * side, side, side)
-        }
-    }
-    for (let index = 0; index < grassArr.length; index++) {
-        grassArr[index].mul();
-    }
-    for (let index = 0; index < grassEaterArr.length; index++) {
-        grassEaterArr[index].eat();
-    }
-    for (let index = 0; index < predatorArr.length; index++) {
-        predatorArr[index].eat();
-    }
-    for (let index = 0; index < lavaArr.length; index++) {
-        lavaArr[index].mul();
-    }
-    for (let index = 0; index < hrshejArr.length; index++) {
-        hrshejArr[index].eat();
     }
 }
